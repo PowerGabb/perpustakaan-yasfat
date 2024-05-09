d8c33613c160a4ddd44f6ea05747e6b822e9060c
Muhammad Panji <muhammadpanjiddd@gmail.com>
2024-05-09 03:36:11 -0700

    Membuat Database Migration Laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('nisn')->nullable();
+            $table->string('class')->nullable();
             $table->string('email')->unique();
+            $table->string('role')->nullable()->default('menunggu aktivasi');
+            
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
