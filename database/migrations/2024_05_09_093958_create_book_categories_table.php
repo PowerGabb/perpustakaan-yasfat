25eaa8e3219c13c9a1c89546d1e4c8c045bf17f4
rangga bayu pratama <griyagpm@gmail.com>
2024-05-12 15:19:21 +0700

    Fixxing Database Error

--- database/migrations/2024_05_09_093958_create_book_categories_table.php
+++ database/migrations/2024_05_09_093958_create_book_categories_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
 
             $table->unsignedBigInteger('category_id');
-            $table->foreign('categories_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
